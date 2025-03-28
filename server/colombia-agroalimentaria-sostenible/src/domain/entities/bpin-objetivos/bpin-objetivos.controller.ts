2a3bf46a9341a87d0ea77befb32a13a7a6c9e5fa
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-03-28 09:42:29 -0500

    ✨ feat(bpin-objetivos): Add filtering capability to findFichaBPIN method and implement new DTO for objective filtering

--- server/colombia-agroalimentaria-sostenible/src/domain/entities/bpin-objetivos/bpin-objetivos.controller.ts
+++ server/colombia-agroalimentaria-sostenible/src/domain/entities/bpin-objetivos/bpin-objetivos.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
 import { BpinObjetivosService } from './bpin-objetivos.service';
 import { ResponseUtils } from '../../shared/utils/response.utils';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { isEmpty } from '../../shared/utils/object.utils';
 
 @ApiTags('Ficha BPIN')
 @ApiBearerAuth()
@@ -9,11 +10,38 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class BpinObjetivosController {
   constructor(private readonly bpinObjetivosService: BpinObjetivosService) {}
 
+  @ApiQuery({
+    name: 'objectivo',
+    required: false,
+    description: 'Objectivo to filter the results',
+    type: String,
+  })
+  @Get('ficha-bpin')
+  async findFichaBPIN(@Query('objectivo') objetivo: string) {
+    console.log(
+      'objectivo',
+      !isEmpty(objetivo) ? objetivo?.split(',').map((id) => Number(id)) : null,
+    );
+    return this.bpinObjetivosService
+      .findFichaBPIN({
+        objectiveId: !isEmpty(objetivo)
+          ? objetivo?.split(',').map((id) => Number(id))
+          : null,
+      })
+      .then((res) =>
+        ResponseUtils.format({
+          description: `Find all BPIN objectives`,
+          data: res,
+          status: HttpStatus.OK,
+        }),
+      );
+  }
+
   @Get()
-  async findFichaBPIN() {
-    return this.bpinObjetivosService.findFichaBPIN().then((res) =>
+  async findObjetivos() {
+    return this.bpinObjetivosService.findObjetivos().then((res) =>
       ResponseUtils.format({
-        description: `Find all BPIN objectives`,
+        description: `Find all objectives`,
         data: res,
         status: HttpStatus.OK,
       }),
