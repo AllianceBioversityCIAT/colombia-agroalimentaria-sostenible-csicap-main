db4f2bb374974d8d115be29c72b184f703aaf859
nhiguita-0526 <n.higuita@cgiar.org>
2025-04-01 16:25:29 -0500

    ✨ feat(gcf-ejes): Implement findEjes endpoint.

--- server/colombia-agroalimentaria-sostenible/src/domain/entities/gcf-ejes/gcf-ejes.controller.ts
+++ server/colombia-agroalimentaria-sostenible/src/domain/entities/gcf-ejes/gcf-ejes.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { GcfEjesService } from './gcf-ejes.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ResponseUtils } from '../../shared/utils/response.utils';
 
+@ApiTags('GCF Ejes')
+@ApiBearerAuth()
 @Controller('gcf-ejes')
 export class GcfEjesController {
   constructor(private readonly gcfEjesService: GcfEjesService) {}
+
+  @Get('ejes_id')
+  async findEjes() {
+    return this.gcfEjesService.findEjes().then(res => ResponseUtils.format({
+      data: res,
+      description: 'Ejes encontrados correctamente',
+      status: HttpStatus.OK,
+    }))
+  }
 }
--- server/colombia-agroalimentaria-sostenible/src/domain/entities/gcf-ejes/gcf-ejes.service.ts
+++ server/colombia-agroalimentaria-sostenible/src/domain/entities/gcf-ejes/gcf-ejes.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { GcfEje } from './entities/gcf-eje.entity';
+import { dataSource } from '../../../db/config/mysql/orm.config';
 
 @Injectable()
-export class GcfEjesService {}
+export class GcfEjesService {
+    private readonly ejeRepository: Repository<GcfEje>;
+    constructor(private readonly dataSource: DataSource) {
+        this.ejeRepository = dataSource.getRepository(GcfEje);
+    }
+
+    async findEjes() {
+        return this.ejeRepository
+            .createQueryBuilder('eje')
+            .select([
+                'eje.id',
+                'eje.nombre',
+            ])
+            .getMany();
+    }
+}
