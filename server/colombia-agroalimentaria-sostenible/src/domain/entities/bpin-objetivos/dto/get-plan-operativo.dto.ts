b1b984bd0009c08b2f806dc7039c2bee92d3aa3e
nhiguita-0526 <n.higuita@cgiar.org>
2025-05-16 10:55:49 -0500

    ✨ feat(bpin-objetivos): Update query parameters to be optional in controller and DTO; enhance getPlanOperativoSocio to return objetivos alongside structured data.

--- server/colombia-agroalimentaria-sostenible/src/domain/entities/bpin-objetivos/dto/get-plan-operativo.dto.ts
+++ server/colombia-agroalimentaria-sostenible/src/domain/entities/bpin-objetivos/dto/get-plan-operativo.dto.ts
@@ -1,22 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class GetPlanOperativoDto {
-  @IsString()
-  objetivo: string;
+  @IsOptional()
+  @ApiProperty({
+    required: false,
+    description: 'Objetivo id',
+    type: String,
+    default: '1',
+  })
+  public objetivo?: string;
 
   @IsOptional()
-  @IsString()
+  @ApiProperty({
+    required: false,
+    description: 'Actividad id',
+    type: String,
+  })
   actividad_id?: string;
 
   @IsOptional()
-  @IsString()
+  @ApiProperty({
+    required: false,
+    description: 'Subactividad id',
+    type: String,
+  })
   subactividad_id?: string;
 
   @IsOptional()
-  @IsString()
+  @ApiProperty({
+    required: false,
+    description: 'Eje id',
+    type: String,
+  })
   eje_id?: string;
 
   @IsOptional()
-  @IsString()
+   @ApiProperty({
+    required: false,
+    description: 'Producto id',
+    type: String,
+  })
   producto_id?: string;
 }
